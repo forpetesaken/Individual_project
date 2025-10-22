e9117fd1fc93bf455d1c5715433b006cad759903
Tectonius <alexbrazil2001@gmail.com>
2025-10-22 00:17:06 -0500

    fixed run start

--- AI_in_Health/run_start.sh
+++ AI_in_Health/run_start.sh
@@ -1,13 +1,13 @@
-%cd /content
-!git clone https://github.com/forpetesaken/Individual_project.git
-%cd /content/Individual_project
-!source .venv/bin/activate
+#!/bin/bash
+git clone https://github.com/forpetesaken/Individual_project.git
+cd Individual_project || exit
+source virtual_env/bin/activate
 
 ## Exploratory Data Analysis
-!/usr/local/bin/python "/content/Individual_project/AI_in_Health/eda.py" --csv "/content/Individual_project/AI_in_Health/training.validation_data.csv"
+/usr/local/bin/python "./AI_in_Health/eda.py" --csv "./AI_in_Health/training.validation_data.csv"
 
 ## Running model
-!/usr/local/bin/python "/content/Individual_project/AI_in_Health/compression_onset.py" --csv "/content/Individual_project/AI_in_Health/training.validation_data.csv"
+/usr/local/bin/python "./AI_in_Health/compression_onset.py" --csv "./AI_in_Health/training.validation_data.csv" --out "./AI_in_Health/output_model_bundle.joblib"
 
 ## Testing model
-!/usr/local/bin/python "/content/Individual_project/AI_in_Health/compression_onset_test.py" --bundle "/content/Individual_project/model_bundle.joblib" --test_csv "/content/Individual_project/AI_in_Health/testing_data.csv"
+/usr/local/bin/python "./AI_in_Health/compression_onset_test.py" --bundle "./AI_in_Health/output_model_bundle.joblib" --test_csv "./AI_in_Health/testing_data.csv"
